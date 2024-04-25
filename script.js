2b42111e9b650ae38f6894b2818ed323cf66164a
IsmailAbb <81118929+IsmailAbb@users.noreply.github.com>
2024-04-25 14:57:38 +0200

    Add files via upload
    base files

---
+++ script.js
@@ -0,0 +1,4 @@
+// This is just an example, you can add more JavaScript functionality as needed
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Document loaded.');
+});
